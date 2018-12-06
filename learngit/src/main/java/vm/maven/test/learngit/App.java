c0f4571799dbcea75be661a7da4c2ce146fe0883
Mothey <vijayendharm@yahoo.co.in>
2018-12-06 16:55:07 -0600

    1st commit

---
+++ learngit/src/main/java/vm/maven/test/learngit/App.java
@@ -0,0 +1,13 @@
+package vm.maven.test.learngit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
